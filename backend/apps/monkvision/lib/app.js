8487d8f7d222fe3ec832fd0eb378669176fb94af
Manmeet Saini <manmeet.saini@deeplogictech.com>
2021-01-27 22:06:33 +0530

    DB init on app call.

--- backend/apps/monkvision/lib/app.js
+++ backend/apps/monkvision/lib/app.js
@@ -2,4 +2,4 @@
  * (C) 2020 TekMonks. All rights reserved.
  */
 
-module.exports.initSync = _ => global.APP_CONSTANTS = require(`${__dirname}/../apis/lib/constants.js`);
+module.exports.initSync = _ => { global.APP_CONSTANTS = require(`${__dirname}/../apis/lib/constants.js`); require(`${APP_CONSTANTS.LIB_DIR}/db.js`).init(); }
