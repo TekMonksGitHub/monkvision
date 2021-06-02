82a4814f2caddadb618f8f2a248378e607e67cfb
sridharan-dlt <77769990+sridharan-dlt@users.noreply.github.com>
2021-06-02 09:34:36 +0530

    Export CSV feature (#33)
    * export csv feature added

---
+++ frontend/apps/monkvision/components/chart-box/lib/utils.mjs
@@ -0,0 +1,28 @@
+/**
+ * Chart-box component common utility functions
+ *  
+ * (C) 2020 TekMonks. All rights reserved.
+ * License: See enclosed license.txt file.
+ */
+
+/**
+ * Export CSV
+ * @param {object} contents The incoming data object for the csv
+ * @param {string} filename The file name of csv
+ */
+
+const _init = async _ => { if (!window.Papa || !window.Papa.unparse) await $$.require(`${APP_CONSTANTS.COMPONENTS_PATH}/chart-box/3p/papaparse-v5.3.0.min.js`); }
+
+async function exportCSV(contents, filename) {
+    await _init();
+    let headers = ["Timestamp"]; headers = headers.concat(contents.legends || "");
+    const rows = []; for (let i = 0; i < contents.length; i++) {
+        const rowContent = [contents.x[i]]; for (let j = 0; j < contents.ys.length; j++) rowContent.push(contents.ys[j][i]);
+        rows.push(rowContent);
+    }
+    const csvBlob = new Blob([Papa.unparse({ "fields": headers, "data": rows })], { type: "text/csv" }), link = document.createElement("a");
+    link.download = filename; link.href = window.URL.createObjectURL(csvBlob);
+    link.click(); window.URL.revokeObjectURL(link.href); link.remove();
+}
+
+export const utils = { exportCSV };
